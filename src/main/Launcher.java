cefc53b309a0ecb1c270834da50b49c68bb8e846
ayancruz <ayan.cruz@jpi.com.ph>
2013-09-20 10:43:42 +0800

    Initial Commit

---
+++ src/main/Launcher.java
@@ -0,0 +1,13 @@
+package main;
+
+import java.io.BufferedReader;
+
+public class Launcher {
+	private static BufferedReader consoleReader;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
